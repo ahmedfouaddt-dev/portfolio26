bfe755393a93513aca3ffdd13f94e120ba910a91
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2026-01-25 11:13:36 +0200

    adding gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export", // Required for static export (for GitHub Pages)
   // Only apply basePath/assetPrefix in production (for GitHub Pages).
-  basePath: isProd ? "/portfolio25" : "",
-  assetPrefix: isProd ? "/portfolio25/" : "",
+  basePath: isProd ? "/portfolio26" : "",
+  assetPrefix: isProd ? "/portfolio26/" : "",
   images: {
     unoptimized: true, // GitHub Pages doesn’t support Next.js image optimization
     domains: ["i.postimg.cc"],
