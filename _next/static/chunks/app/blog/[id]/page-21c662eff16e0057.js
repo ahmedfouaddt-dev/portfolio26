45af94a3328f82c5fa07b704027d84ed995461ad
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2026-01-25 11:19:01 +0200

    Updates

---
+++ _next/static/chunks/app/blog/[id]/page-21c662eff16e0057.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[548],{9986:function(n,e,u){Promise.resolve().then(u.bind(u,9940))}},function(n){n.O(0,[347,940,971,69,744],function(){return n(n.s=9986)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/index-2/page-69617faeed9649e3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[927],{},function(n){n.O(0,[347,885,126,940,687,971,69,744],function(){return n(n.s=3687)}),_N_E=n.O()}]);
